f4b6f1ede2557ca15709ad375bc1b6e816296d77
crpoudyal <crpoudyal@gmail.com>
2022-04-06 08:37:56 +0545

    listview

--- app/src/main/java/com/example/sagarmatha/ListExampleActivity.java
+++ app/src/main/java/com/example/sagarmatha/ListExampleActivity.java
@@ -6,15 +6,38 @@ import android.widget.ListView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class ListExampleActivity extends AppCompatActivity {
-    public ListView lvText;
+    public ListView lvTest;
+    public ArrayList<ListData> listItems;
+    private ListExampleAdapter mAdapter;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_view_layout);
 
-        lvText = findViewById(R.id.lv_text);
+        lvTest = findViewById(R.id.lv_text);
 
+        listItems = new ArrayList<>();
+        listItems.add(new ListData("Android","This is description for title"));
+        listItems.add(new ListData("Android","This is description for title"));
+        listItems.add(new ListData("Android","This is description for title"));
+        listItems.add(new ListData("Android","This is description for title"));
+        listItems.add(new ListData("Android","This is description for title"));
+        listItems.add(new ListData("Android","This is description for title"));
+        listItems.add(new ListData("Android","This is description for title"));
+        listItems.add(new ListData("Android","This is description for title"));
+        listItems.add(new ListData("Android","This is description for title"));
+        listItems.add(new ListData("Android","This is description for title"));
+        listItems.add(new ListData("Android","This is description for title"));
+        mAdapter = new ListExampleAdapter(this,listItems);
+
+        lvTest.setAdapter(mAdapter);
+
+        listItems.add(new ListData("new title","This is description for title"));
+
+        mAdapter.notifyDataSetChanged();
 
     }
 }
--- app/src/main/java/com/example/sagarmatha/ListExampleAdapter.java
+++ app/src/main/java/com/example/sagarmatha/ListExampleAdapter.java
@@ -11,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ListExampleAdapter extends ArrayAdapter<ListData> {
 
@@ -18,18 +19,47 @@ public class ListExampleAdapter extends ArrayAdapter<ListData> {
 
     private  Context mContext;
     private LayoutInflater mInflater;
+    private ArrayList<ListData> listData;
 
     public ListExampleAdapter(@NonNull Context context,  @NonNull ArrayList<ListData> listItems) {
         super(context,R.layout.list_item_view, listItems);
         this.mContext = context;
         this.mInflater = LayoutInflater.from(mContext);
+        this.listData = listItems;
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        ViewHolder viewHolder;
+
+        ListData item = listData.get(position);
+
+        if(convertView == null){
+            convertView = mInflater.inflate(R.layout.list_item_view,parent,false);
+            viewHolder = new ViewHolder();
+
+            viewHolder.tvDescription = convertView.findViewById(R.id.tv_des);
+            viewHolder.tvTitle = convertView.findViewById(R.id.tv_header);
+
+            convertView.setTag(viewHolder);
+
+        }else{
+          viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.tvTitle.setText(item.getTitle());
+        viewHolder.tvDescription.setText(item.getDescription());
+
+        return convertView;
+
     }
+
+    @Override
+    public int getCount() {
+        return listData.size();
+    }
+
     public  class ViewHolder{
         public TextView tvTitle,tvDescription;
     }
