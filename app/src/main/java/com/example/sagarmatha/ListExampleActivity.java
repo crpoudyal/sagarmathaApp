04bb53e591336f22c6069754cf884d87a789d164
crpoudyal <crpoudyal@gmail.com>
2022-04-06 16:54:36 +0545

    add btn to add new list in listview

--- app/src/main/java/com/example/sagarmatha/ListExampleActivity.java
+++ app/src/main/java/com/example/sagarmatha/ListExampleActivity.java
@@ -1,14 +1,20 @@
 package com.example.sagarmatha;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
-
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import java.util.ArrayList;
 
 public class ListExampleActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
     public ListView lvTest;
     public ArrayList<ListData> listItems;
     private ListExampleAdapter mAdapter;
@@ -17,27 +23,43 @@ public class ListExampleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_view_layout);
 
+
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         lvTest = findViewById(R.id.lv_text);
 
         listItems = new ArrayList<>();
-        listItems.add(new ListData("Android","This is description for title"));
-        listItems.add(new ListData("Android","This is description for title"));
-        listItems.add(new ListData("Android","This is description for title"));
-        listItems.add(new ListData("Android","This is description for title"));
-        listItems.add(new ListData("Android","This is description for title"));
-        listItems.add(new ListData("Android","This is description for title"));
-        listItems.add(new ListData("Android","This is description for title"));
-        listItems.add(new ListData("Android","This is description for title"));
-        listItems.add(new ListData("Android","This is description for title"));
         listItems.add(new ListData("Android","This is description for title"));
         listItems.add(new ListData("Android","This is description for title"));
         mAdapter = new ListExampleAdapter(this,listItems);
 
         lvTest.setAdapter(mAdapter);
 
-        listItems.add(new ListData("new title","This is description for title"));
+    }
 
-        mAdapter.notifyDataSetChanged();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.lv_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_add:
+                listItems.add(new ListData("New title","This is description for title"));
+                mAdapter.notifyDataSetChanged();
+                break;
+            case android.R.id.home:
+                finish();
+                break;
+            default:
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
