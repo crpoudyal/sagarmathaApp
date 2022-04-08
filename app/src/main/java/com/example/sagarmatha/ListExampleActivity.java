4cc74c648187779f20afdbd322956a900e593651
crpoudyal <crpoudyal@gmail.com>
2022-04-08 09:01:22 +0545

    dialog box in android

--- app/src/main/java/com/example/sagarmatha/ListExampleActivity.java
+++ app/src/main/java/com/example/sagarmatha/ListExampleActivity.java
@@ -1,11 +1,17 @@
 package com.example.sagarmatha;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -24,6 +30,8 @@ public class ListExampleActivity extends AppCompatActivity {
         setContentView(R.layout.list_view_layout);
 
 
+
+
         toolbar = findViewById(R.id.toolbar);
 
         setSupportActionBar(toolbar);
@@ -38,6 +46,41 @@ public class ListExampleActivity extends AppCompatActivity {
         mAdapter = new ListExampleAdapter(this,listItems);
 
         lvTest.setAdapter(mAdapter);
+        lvTest.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                Toast.makeText(ListExampleActivity.this, "Item Clicked "+position, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        lvTest.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long l) {
+              AlertDialog.Builder builder = new  AlertDialog.Builder(ListExampleActivity.this).setTitle("Warning").
+                      setMessage("Did you want to Delete?").
+                      setCancelable(false).
+                      setNegativeButton("No", new DialogInterface.OnClickListener() {
+                  @Override
+                  public void onClick(DialogInterface dialogInterface, int i) {
+                      dialogInterface.dismiss();
+                  }
+              }).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                  @Override
+                  public void onClick(DialogInterface dialogInterface, int i) {
+                      listItems.remove(position);
+                      mAdapter.notifyDataSetChanged();
+                      dialogInterface.dismiss();
+                  }
+              });
+
+              AlertDialog dialog = builder.create(); //Create only not display
+                dialog.show();
+                return true;
+            }
+        });
+
 
     }
 
@@ -67,4 +110,5 @@ public class ListExampleActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
